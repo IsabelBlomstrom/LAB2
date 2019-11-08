3ad5ae86b271cd5bd5ba13cac94fa777f5f8328c
Isabel Blomström <isabel.blomstrom@gmail.com>
2019-11-08 11:56:20 +0100

    Lagt till togglemeny

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+$(document).ready(function() {
+
 VANTA.BIRDS({
     el: "#vantajs",
     birdSize: 0.5,
-    wingSpan: 1.00,
+    wingSpan: 2.00,
     speedLimit: 1.00,
+    separation: 60.00,
+    cohesion: 30.00
   })
+  
+
+  $( "nav" ).hide();
+
+  $('.fa-bars').click(function(){
+      $('nav').toggle('slow')
+  })
+
+});
+
+ 
   
   
