7d631beb22888ae45fca675f5c35be9cee04b03b
Isabel Blomström <isabel.blomstrom@gmail.com>
2020-11-25 09:08:02 +0100

    Removed js library that almost made computer crash

--- script.js
+++ script.js
@@ -3,24 +3,10 @@
   * Starts when the page is ready
   */
  $(document).ready(function () {
-     backgroundAnimation()
      $(".hiddenUl").hide();
      addEventListeners()
  })
 
- /**
-  * Holds the object for the background animation
-  */
- function backgroundAnimation() {
-     VANTA.BIRDS({
-         el: "#vantajs",
-         birdSize: 0.5,
-         wingSpan: 15,
-         speedLimit: 1.00,
-         separation: 60.00,
-         cohesion: 30.00
-     })
- }
 
 /**
  * Handles the events that shows/hides the hidden UL when clicked
